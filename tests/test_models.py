10a9ea3093cc45f1377cd17c77ddd7ae05d7ce40
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-09-20 15:15:59 +0200

    more parameterization of test

--- tests/test_models.py
+++ tests/test_models.py
@@ -215,14 +215,17 @@ def test_transform():
       'z': {'x': 3.0, 'y': 0.0, 'z': 1.0},
       }
   affine_str = f"{aff['x']['x']} {aff['x']['y']} {aff['x']['z']} {trs['x']} {aff['y']['x']} {aff['y']['y']} {aff['y']['z']} {trs['y']} {aff['z']['x']} {aff['z']['y']} {aff['z']['z']} {trs['z']}"
+  name = "Translation to Nominal Grid"
   transform_xml = (
       '<ViewTransform type="affine">'
-      '<Name>Translation to Nominal Grid</Name>'
+      f'<Name>{name}</Name>'
       f'<affine>{affine_str}</affine>'
       '</ViewTransform>'
     )
 
   tx_model = AffineViewTransform.from_xml(transform_xml)
+  assert tx_model.name== name
+  assert tx_model.typ == 'affine'
   tx = tx_model.to_transform()
   assert tx.transform == HoAffine(affine=aff, translation=trs)
   assert stringify_tuple(map(str, flatten_hoaffine(tx.transform, axes_out=('x','y','z')))) == affine_str
